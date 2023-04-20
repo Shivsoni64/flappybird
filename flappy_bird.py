00ddf0407faef52749bfc23422f8d45ec2940bc3
Shivsoni64 <91825122+Shivsoni64@users.noreply.github.com>
2023-04-20 16:42:35 +0530

    Add files via upload

--- flappy_bird.py
+++ flappy_bird.py
@@ -1,19 +1,11 @@
-"""
-The classic game of flappy bird. Make with python
-and pygame. Features pixel perfect collision using masks :o
-
-Date Modified:  Jul 30, 2019
-Author: Tech With Tim
-Estimated Work Time: 5 hours (1 just for that damn collision)
-"""
-import os
-import pickle
-import random
-import time
+ 
 import pygame
-
+import random
+import os
+import time
+import neat
 import visualize
-
+import pickle
 pygame.font.init()  # init font
 
 WIN_WIDTH = 600
